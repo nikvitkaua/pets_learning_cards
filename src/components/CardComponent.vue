e428f658e42667a8ab73dc6adbf73e74f0707a86
Mykyta Paziuka <nikvitka.it@gmail.com>
2025-02-05 10:42:08 +0200

    create flip animation to card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -8,26 +8,31 @@ const props = defineProps({
   },
 });
 
-const showAnswer = ref(false);
+const isFlipped = ref(false);
 
-const toggleAnswer = () => {
-  showAnswer.value = !showAnswer.value;
+const toggleCard = () => {
+  isFlipped.value = !isFlipped.value;
 };
 </script>
 
 <template>
-  <div class="card" @click="toggleAnswer">
-    <h3 class="card__title">Question #{{ question.id }}</h3>
-    <h2 class="card__question">{{ question.question }}</h2>
-    <p class="card__answer" v-if="showAnswer">{{ question.answer }}</p>
+  <div class="card" @click="toggleCard">
+    <div class="card__inner" :class="{ 'is-flipped': isFlipped }">
+      <div class="card__front">
+        <h3 class="card__title">Питання #{{ question.id }}</h3>
+        <h2 class="card__question">{{ question.question }}</h2>
+      </div>
+      <div class="card__back">
+        <h3 class="card__title">Відповідь</h3>
+        <h2 class="card__question">{{ question.question }}</h2>
+        <p class="card__answer">{{ question.answer }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="sass">
 .card
-  position: relative
-  background-color: black
-  border-radius: 16px
   padding: 20px
   color: #fff
   display: flex
@@ -35,6 +40,39 @@ const toggleAnswer = () => {
   justify-content: center
   align-items: center
   cursor: grab
+  height: 80vh
+
+  &__inner
+    transform-style: preserve-3d
+    transition: transform 0.6s
+    position: relative
+    width: 100%
+    height: 100%
+    border-radius: 16px
+
+    &.is-flipped
+      transform: rotateY(180deg)
+
+  &__front, &__back
+    position: absolute
+    width: 100%
+    height: 100%
+    min-height: 200px
+    background-color: #000
+    border-radius: 16px
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    color: #fff
+    backface-visibility: hidden
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3)
+  
+  &__front
+    z-index: 2
+
+  &__back
+    transform: rotateY(180deg)
 
   &__title
     position: absolute
@@ -46,6 +84,7 @@ const toggleAnswer = () => {
     text-align: center
 
   &__answer
+    max-width: 700px
     text-align: center
     line-height: 1.5em
 </style>
